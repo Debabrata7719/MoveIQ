9378672691a46fe770dd3b931d069ad894204d67
Debabrata Dey <debabratadey8080@gmail.com>
2026-08-02 00:30:06 +0530

    Update Celery beat scheduler timezone to IST

--- src/worker/celery_app.py
+++ src/worker/celery_app.py
@@ -22,8 +22,8 @@ celery_app.conf.update(
     task_serializer="json",
     accept_content=["json"],
     result_serializer="json",
-    timezone="UTC",
-    enable_utc=True,
+    timezone="Asia/Kolkata",
+    enable_utc=False,
     broker_connection_retry_on_startup=True,
 )
 
