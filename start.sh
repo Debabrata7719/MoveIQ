f92ea7e2f2542794976948fbe012c40e55969ab7
Debabrata Dey <debabratadey8080@gmail.com>
2026-08-01 19:20:28 +0530

    Add startup script for single-instance deployment

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Start Celery worker in the background
+celery -A src.worker.celery_app worker --loglevel=info --concurrency=1 &
+
+# Start FastAPI server in the foreground
+uvicorn api.server:app --host 0.0.0.0 --port ${PORT:-10000}
